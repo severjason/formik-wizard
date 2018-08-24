b766a711615746728106dcc9b66d460aceb2d79b
severjason <ssever2010@gmail.com>
2018-08-24 16:03:51 +0300

    removed redux

--- src/store.js
+++
@@ -1,6 +0,0 @@
-import rootReducer from './reducers';
-import { createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(rootReducer, composeWithDevTools());
-export default store;
