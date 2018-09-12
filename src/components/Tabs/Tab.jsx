4637db14e99472bd4e220d19419073d8caf54ecb
severjason <ssever2010@gmail.com>
2018-09-12 09:52:11 +0300

    added modal to tab

--- src/components/Tabs/Tab.jsx
+++ src/components/Tabs/Tab.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
 
 const _TabProps = {
   type: PropTypes.string,
@@ -23,15 +24,33 @@ class Tab extends React.Component {
     this.state = {
       type: this.props.type,
       reservations: this.props.reservations,
+      modal: false,
+      activeId: '',
+      inputValue: '',
     }
 
   }
 
-  handleItemDelete = (id) => {
-    const {reservations} = this.state;
-    const filtered = reservations.filter((item) => item.id !== id);
-    this.setState(() => ({reservations: filtered}));
+  handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue >=0 && inputValue <= 12) {
+      this.setState(() => ({inputValue}))
+    }
   }
+
+  handleCancel = () => {
+    console.log('canceled!');
+  }
+
+  handleItemDelete = () => {
+    const {reservations, activeId} = this.state;
+    const filtered = reservations.filter((item) => item.id !== activeId);
+    this.setState({reservations: filtered}, () => this.closeModal());
+  }
+
+  openModal = (id) => this.setState(() => ({modal: true, activeId: id}));
+
+  closeModal = () => this.setState(() => ({modal: false, activeId: ''}));
 
   getHeadings() {
     const {type} = this.state;
@@ -51,8 +70,15 @@ class Tab extends React.Component {
     )
   }
 
+  getActions(id) {
+    return <React.Fragment>
+      <a href="#" onClick={() => this.openModal(id)}>Manage</a>
+      <a href="#" onClick={this.handleCancel}>Cancel</a>
+    </React.Fragment>
+  }
+
   getTableData() {
-    const { reservations } = this.state;
+    const { reservations, type } = this.state;
     return  (reservations.length === 0)
       ? <tr>
       <td colSpan={8}>
@@ -68,19 +94,32 @@ class Tab extends React.Component {
           <th>{item.reservation_time}</th>
           <th>{item.party_size}</th>
           <th>{item.special_requests}</th>
-          <th
-            onClick={() => this.handleItemDelete(item.id)}
-          >{item.actions}</th>
+          <th>{type === 'canceled' ? item.name : this.getActions(item.id)}</th>
         </tr>
       ))
 
   }
 
-
   render() {
-    const {type} = this.state;
+    const {type, modal, inputValue} = this.state;
     return (
       <div id={type} className="tab-pane " role="tabpanel">
+        <Modal isOpen={modal} toggle={this.closeModal}>
+          <ModalHeader>Modal title</ModalHeader>
+          <ModalBody>
+            <Input
+              type="number"
+              min="0"
+              max="12"
+              value={inputValue}
+              onChange={this.handleInputChange}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.handleItemDelete}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.closeModal}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
         <div className="table-responsive">
           <table className="table">
             {this.getHeadings()}
